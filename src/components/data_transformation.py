e7b9c6797c4716e95ba2da4e347832e1fa895afd
Ayush Kumar <mailforayush@gmail.com>
2024-04-07 11:44:20 +0530

    prediction pipeline

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -103,7 +103,8 @@ class DataTransformation:
             train_arr=np.c_[
                 input_feature_train_arr, np.array(target_feature_train_df)
             ]
-            test_arr= np.c_[input_feature_test_arr, np.array(target_feature_test_df)
+            test_arr= np.c_[
+                input_feature_test_arr, np.array(target_feature_test_df)
             ]
 
             logging.info(f" saved preprocessing object")
